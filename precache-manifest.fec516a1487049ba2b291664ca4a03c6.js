812119ef99508ba0bf4f1fafecb1895cc1a94a98
Andrew Hanson <andrew@spotix.com>
2021-12-10 12:35:29 -0600

    Updates

---
+++ precache-manifest.fec516a1487049ba2b291664ca4a03c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2b6acf791735e5f1fe7d088f395dfb",
+    "url": "/photo-plbum/index.html"
+  },
+  {
+    "revision": "f76cfe76a5e6b3de38bf",
+    "url": "/photo-plbum/static/css/main.5e8a80b5.chunk.css"
+  },
+  {
+    "revision": "797e78400f15e73a1da1",
+    "url": "/photo-plbum/static/js/2.41557d2d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/photo-plbum/static/js/2.41557d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76cfe76a5e6b3de38bf",
+    "url": "/photo-plbum/static/js/main.29e3d8ee.chunk.js"
+  },
+  {
+    "revision": "c55ea771308d957d138a",
+    "url": "/photo-plbum/static/js/runtime-main.c8705d9d.js"
+  }
+]);
